4c72292341c7576c979ee9f25659c4088ee272c7
MatasBudriunas <zewas1m@gmail.com>
2023-03-04 09:55:42 +0700

    manually prompt ai content

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        <h1>{{ output }}</h1>
+    <div class="question-block">
+        <label>Ask a question for content generation: </label>
+        <input v-model="input"
+               type="text"
+               required
+        >
+        <button @click="getAIResponse">Submit</button>
+    </div>
+    <div v-if="answerProvided" class="answer-block">
+        {{ output }}
     </div>
 </template>
 
@@ -10,21 +18,24 @@ export default {
     data() {
         return {
             output: null,
+            input: null,
+            answerProvided: false
         }
-    },
-    created() {
-        this.getAIResponse();
     },
     methods: {
         getAIResponse() {
             axios.get('/api/completion').then((resp) => {
                 this.output = resp.data.output
             });
+
+            this.answerProvided = true;
         }
     }
 }
 </script>
 
 <style scoped>
-
+    .question-block, .answer-block {
+        text-align: center;
+    }
 </style>
