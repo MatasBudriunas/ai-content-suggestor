c127ad462b3ec3e1880f8c6871a5442fba760381
MatasBudriunas <zewas1m@gmail.com>
2023-03-15 07:40:26 +0700

    spinner in homepage, request flow adjustment

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap';
 import 'popper.js';
+import 'jQuery';
 
 import {createApp} from 'vue'
 import {vue3Spinner} from 'vue3-spinner'
 
 import Home from './Home.vue'
 
-createApp(Home)
-    .use(vue3Spinner)
-    .mount("#home")
+const app = createApp(Home)
+
+app.component('vue3-spinner', vue3Spinner)
+
+createApp(Home).mount("#home")
