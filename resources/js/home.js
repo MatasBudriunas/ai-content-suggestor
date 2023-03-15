a0901f1111bf74e6aefea83d79fd1c05daaf7c6f
MatasBudriunas <zewas1m@gmail.com>
2023-03-16 06:51:46 +0700

    fix multiple jQuery imports

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap';
 import 'popper.js';
-import 'jQuery';
 
 import {createApp} from 'vue'
 import {vue3Spinner} from 'vue3-spinner'
