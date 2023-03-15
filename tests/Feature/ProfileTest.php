145224bde8775f778fde9e51d41d1536bd46b4ab
MatasBudriunas <zewas1m@gmail.com>
2023-03-16 06:49:52 +0700

    feature tests

---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class ProfileTest extends TestCase
+{
+    public function test_example()
+    {
+        $response = $this->get('/profile');
+
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_profile_completion() {
+        $response = $this->call('GET', '/api/completion', [
+            'input' => 'Is the sky blue?',
+            'type' => 'generate'
+        ]);
+
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_profile_completion_without_input() {
+        $response = $this->call('GET', '/api/completion');
+
+        $response->assertSessionHasErrors('input');
+    }
+}
