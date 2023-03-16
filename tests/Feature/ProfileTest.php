5c9feaba8a718d1c266444adc0355b5c5cf4651b
MatasBudriunas <zewas1m@gmail.com>
2023-03-16 09:09:39 +0700

    formatting tests

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -16,7 +16,8 @@ class ProfileTest extends TestCase
         $response->assertStatus(Response::HTTP_OK);
     }
 
-    public function test_profile_completion() {
+    public function test_profile_completion()
+    {
         $response = $this->call('GET', '/api/completion', [
             'input' => 'Is the sky blue?',
             'type' => 'generate'
@@ -25,7 +26,8 @@ class ProfileTest extends TestCase
         $response->assertStatus(Response::HTTP_OK);
     }
 
-    public function test_profile_completion_without_input() {
+    public function test_profile_completion_without_input()
+    {
         $response = $this->call('GET', '/api/completion');
 
         $response->assertSessionHasErrors('input');
